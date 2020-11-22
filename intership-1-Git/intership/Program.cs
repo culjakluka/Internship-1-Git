ce1e2dcd1a8db8de45a3183c856a11cacf642553
culjakluka <llukaculjak@gmail.com>
2020-11-22 15:45:25 +0100

    added another console output

--- intership-1-Git/intership/Program.cs
+++ intership-1-Git/intership/Program.cs
@@ -7,6 +7,7 @@ namespace intership
         static void Main(string[] args)
         {
             Console.WriteLine("Hello from second commit!");
+            Console.WriteLine("Commit koji je falio.");
         }
     }
 }
